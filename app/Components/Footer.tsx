766334b0f2e270b2439a6eb5d8a65f8381bc9d74
Buttons Technology <buttonsafrotech@gmail.com>
2025-09-19 17:17:06 +0100

    footer done

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='w-full h-[259px] bg-[#F2F2F2] flex flex-col items-center gap-4 pt-8'>
+        <div className="flex flex-col">
+            <Image src="/img/logo.png" alt='logo' height={50} width={100} className='w-[100px] h-[49px]' />
+              <span className="text-[16px] md:text-[20px] font-black">DREAMBOX</span>
+              <span className="text-[8px] md:text-[10px] text-center">Creative-Tech Academy</span>
+            </div>
+            <div className='flex text-[10px] font-bold md:text-[16px] gap-4 md:gap-8'>
+                <span>Home</span>
+                <span>About</span>
+                <span>Programs</span>
+                <span>Contact</span>
+                <span>Privacy Policy</span>
+            </div>
+            <div className='flex gap-4 md:gap-8'>
+                
+            <Image src="/img/linkedin.png" alt='logo' height={50} width={50} className='w-[24px] h-[24px]' />
+            <Image src="/img/instagram.png" alt='logo' height={50} width={50} className='w-[24px] h-[24px]' />
+            <Image src="/img/twitter.png" alt='logo' height={50} width={50} className='w-[24px] h-[24px]' />
+            <Image src="/img/tiktok.png" alt='logo' height={50} width={50} className='w-[24px] h-[24px]' />
+            </div>
+            <div className='text-[10px] flex flex-col '>
+                <span>dreamboxtechacademy@gmail.com | 09066596603</span>
+                <span>@2025 Powered by buttons. All Rights Reserved</span>
+            </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Nav_bar from "./Components/Nav_bar";
 import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
         <>
         <Nav_bar/>
         <Hero/>
+        <Footer />
         </>
   );
 }
