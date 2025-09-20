33016a7d260ce86b8c86956d3f05adfd29abc285
Buttons Technology <buttonsafrotech@gmail.com>
2025-09-20 08:01:49 +0100

    font face fix

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <>
       <div className='w-[365px] mx-auto my-[24px] '>
-        <span className='text-[36px] font-bold '>Unlock Your Child&apos;s Future with Creative Tech. </span><br />
+        <span className='text-[36px] font-bold font-outfit '>Unlock Your Child&apos;s Future with Creative Tech. </span><br />
         <span className='text-[36px] text-[#A40C76]'>
 Coding, Robotics, and Design for the Next Generation of Innovators.</span><br />
         <span className='text-[20px]'>From virtual classrooms to school clubs, we bring fun, hands-on learning right to your doorstep in Lagos.</span>
