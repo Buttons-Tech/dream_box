fde6fcb90b3a3cc8799034e5c12fd7588fa85a09
Buttons Technology <buttonsafrotech@gmail.com>
2025-09-22 10:54:40 +0100

    fixed CTA links

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CTA_button1 from './CTA_button1'
 import CTA_button2 from './CTA_button2'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
@@ -11,10 +12,16 @@ const Hero = () => {
         <span className='text-[20px] text-[#A40C76]'>
 Coding, Robotics, and Design for the Next Generation of Innovators.</span><br />
         {/* <span className='text-[20px]'>From virtual classrooms to school clubs, we bring fun, hands-on learning right to your doorstep in Lagos.</span> */}
+        
         <div className='flex flex-col gap-4 w-full items-center md:flex-row md:justify-center mt-8'>
+<Link href="/register" className='text-[20px] text-[#A40C76] underline'>
           <CTA_button1 />
+          </Link>
+          <Link href="/school-dashboard" className='text-[20px] text-[#A40C76] underline'>
           <CTA_button2 />
+          </Link>
         </div>
+        
         <Image src="/img/hero_img.jpg" alt="Hero Image" width={375} height={300} className='mx-auto mt-8' />
       </div>
     </>
